76152d13e6a9dbe4847024f83f58069d01b7b614
Kapil Adhikari <kapiladhikari2060@gmail.com>
2024-08-24 22:41:43 +0545

    Modified

--- project-based-learning/react_project/react-rmdb/src/App.js
+++ project-based-learning/react_project/react-rmdb/src/App.js
@@ -1,25 +1,10 @@
-import { func } from 'prop-types';
-import React, {useState} from 'react';
+import React from 'react';
 
 function App() {
-  const [index, setIndex] = useState(0);
-  const handleAdd = () => {
-    setIndex(index+1);
-  };
-  const handleSub = () => {
-    setIndex(index-1);
-  };
-  const reset = () => {
-    setIndex(0);
-  };
-
   return (
-    <>
-      <button onClick={handleAdd}>+</button>
-      <button onClick={handleSub}>-</button>
-      <button onClick={reset}>Reset</button>
-      <h2>{index}</h2>
-    </>
+    <div className='App'>
+      Start here
+    </div>
   );
 }
 
